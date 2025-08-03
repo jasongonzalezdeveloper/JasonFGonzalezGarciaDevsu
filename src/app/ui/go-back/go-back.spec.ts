605481fec997f2de8c2fad60786e5ca8097b2697
Jason F. Gonzalez Garcia <jason.gonzalez.developer@gmail.com>
2025-08-03 15:23:05 -0600

    Add go back test

--- src/app/ui/go-back/go-back.spec.ts
+++ src/app/ui/go-back/go-back.spec.ts
@@ -1,16 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
 
 import { GoBack } from './go-back';
+import { provideRouter, Router } from '@angular/router';
+import { routes } from '../../app.routes';
 
-xdescribe('GoBack', () => {
+describe('GoBack', () => {
   let component: GoBack;
   let fixture: ComponentFixture<GoBack>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GoBack]
-    })
-    .compileComponents();
+      imports: [GoBack],
+      providers: [
+        provideRouter(routes)
+      ]
+    }).compileComponents();
+    router = TestBed.inject(Router);
+    await router.navigate(['/add-product']);
 
     fixture = TestBed.createComponent(GoBack);
     component = fixture.componentInstance;
@@ -19,5 +26,19 @@ xdescribe('GoBack', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should start at add-product route', () => {
+    expect(router.url).toEqual('/add-product');
+  });
+
+  it('should click go back button and navigate to previous page', async () => {
+    await new Promise(resolve => setTimeout(resolve, 500));
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+    await new Promise(resolve => setTimeout(resolve, 500));
+    fixture.detectChanges();
+    expect(router.url).toBe('/products');
   });
 });
